e3b79e461f63e886379b63af292f04c51c86ac30
Hunter Johnson <0xhjohnson@protonmail.com>
2019-03-09 22:18:37 -0500

    Added vowel_count kata

---
+++ kata/go/vowel_count.go
@@ -0,0 +1,13 @@
+package kata
+
+import "unicode"
+
+func GetCount(str string) (count int) {
+  for _, char := range str {
+    switch unicode.ToLower(char) {
+    case 'a', 'e', 'i', 'o', 'u':
+      count++
+    }
+  }
+  return count
+}
