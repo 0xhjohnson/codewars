a0fac98a0f1f74700d5d6205f9ebd1c9bbaf9539
Hunter Johnson <0xhjohnson@protonmail.com>
2019-03-10 23:19:21 -0400

    Fixed formatting vowel count kata

--- kata/go/vowel_count.go
+++ kata/go/vowel_count.go
@@ -3,11 +3,11 @@ package kata
 import "unicode"
 
 func GetCount(str string) (count int) {
-  for _, char := range str {
-    switch unicode.ToLower(char) {
-    case 'a', 'e', 'i', 'o', 'u':
-      count++
-    }
-  }
-  return count
+	for _, char := range str {
+		switch unicode.ToLower(char) {
+		case 'a', 'e', 'i', 'o', 'u':
+			count++
+		}
+	}
+	return count
 }
