698b0f8a7045effe6ae6228cd24cda6ad988f149
Hunter Johnson <0xhjohnson@protonmail.com>
2019-03-10 23:17:03 -0400

    Fixed formatting multiple 3 or 5

--- kata/go/multiple_3_5.go
+++ kata/go/multiple_3_5.go
@@ -1,13 +1,13 @@
 package kata
 
 func Multiple3And5(number int) int {
-  sum := 0
-  for i := number - 1; i > 0; i-- {
-    if i%3 == 0 {
-      sum += i
-    } else if i%5 == 0 {
-      sum += i
-    }
-  }
-  return sum
+	sum := 0
+	for i := number - 1; i > 0; i-- {
+		if i%3 == 0 {
+			sum += i
+		} else if i%5 == 0 {
+			sum += i
+		}
+	}
+	return sum
 }
